3e5dfc6c37ef33bf52e9acade8788a9809cbe3d2
yatesdr <derek.yates@live.com>
2024-06-04 05:19:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
-from sanic import Sanic
-from sanic.response import text, json, html, redirect
+from flask import Flask, redirect, request
+#from flask.response import text, json, html, redirect
 from lib.avkans_ndi_utils import AvkansCamera
 from lib.avkans_visca_utils import AvkansControl
 import json as jsonlib
@@ -9,14 +9,18 @@ from queue import Queue
 from ultralytics import YOLO
 import numpy as np
 
+# Mediapipe models via python's face-detection-tflite models
+from fdlite import FaceDetection, FaceDetectionModel
+
 # Instantiate Sanic app
-app = Sanic("ptzTracker")
-app.ctx.db_env=lmdb.open("ptzTracker.db")
+app = Flask("ptzTracker")
+db_env=lmdb.open("ptzTracker.db")
+
 tqueues=list()
 
 # Thread for doing the actual camera tracking commands.
 # This thread assumes a parked zoom, and tracks a single "face".
-def track_single_without_zoom(cam_ip):
+def yolo_tracker(cam_ip):
     print("Single face tracking thread started on ",cam_ip)
 
     model_face = YOLO('yolov8n-face.pt')
@@ -27,7 +31,7 @@ def track_single_without_zoom(cam_ip):
     ptz = AvkansControl(cam_ip)
 
     # For debugging it's useful to start at home.   Disable for prod.
-    ptz.send_raw(ptz.cmd.ptz_home)
+    ptz.send(ptz.cmd.ptz_home)
     ptz.wait_complete()
 
     hafov = ptz.ptz_get_hfov() # horizontal angular field of view.
@@ -41,15 +45,16 @@ def track_single_without_zoom(cam_ip):
     hafov = ptz.ptz_get_hfov() # horizontal angular field of view.
     vafov = 9./16.*hafov # vertical angular field of view, assumes 16:9 aspect ratio and rectilinear lens.
 
-    pid_pan_params = (0.5,0.02,0.)
-    pid_tilt_params = (0.5,0.02,0.)
-
     # Where we want the target in the frame
     target_loc = np.array((frame.shape[1]*target_position[0],frame.shape[0]*target_position[1]))
 
     loop_rate=10 # starting value   
     while(True):
         tstart=time.time()
+
+        # Flush socket messages if they accumulated.
+        while (ptz.recv(blocking=False)):
+            pass
     
         hafov = ptz.ptz_get_hfov() # horizontal angular field of view.
         vafov = 9./16.*hafov # vertical angular field of view, assumes 16:9 aspect ratio and rectilinear lens.
@@ -57,7 +62,7 @@ def track_single_without_zoom(cam_ip):
 
         # To most closely sync the current position with the frame, it's best to get position first
         # position getting has the most latency.
-        cam_abs_pos = ptz.ptz_get_abs_position() # About 50ms or so usually.
+        cam_abs_pos = ptz.ptz_get_position() # About 50ms or so usually.
         if cam_abs_pos==None: # Sometimes the socket flakes.
             continue
         frame_ts=time.time()
@@ -97,20 +102,125 @@ def track_single_without_zoom(cam_ip):
             # Continuous motion means some responses may get queued up, flush them.
             #ptz.socket_flush()
 
-            ptz.send_raw(ptz.cmd.ptz_to_abs_position(cam_correct_pos[0],cam_correct_pos[1],pan_speed,tilt_speed))
+            ptz.send(ptz.cmd.ptz_to_abs_position(cam_correct_pos[0],cam_correct_pos[1],pan_speed,tilt_speed))
             #ptz.wait_complete()
 
-            
-
-
         loop_rate=1/(time.time()-tstart)
-        print("Loop rate: ",loop_rate," Hz")
+        print("\rLoop rate: ",loop_rate," Hz",end=None)
 
         if haltTrackingThread(cam_ip):
             exit()
 
+# A tracking thread using google mediapipe.
+def mediapipe_tracker(cam_ip, zoom=False):
+    print("MediaPipe tracker thread started on ",cam_ip)
+
+    detect_faces = FaceDetection(model_type=FaceDetectionModel.FULL)
+
+    ndi = AvkansCamera()
+    ndi.connect_by_ip(cam_ip)
+    frame = ndi.get_cv2_frame()  # First frame takes a second or two, pull it at inits.
+    ptz = AvkansControl(cam_ip) # Visca controller.
+
+    # For debugging it's useful to start at home.   Disable for prod.
+    ptz.send(ptz.cmd.ptz_zero_zero)
+    ptz.wait_complete()
+
+    hafov = ptz.ptz_get_hfov() # horizontal angular field of view.
+    print("hafov 1: ",hafov)
+
+    error_vector=(None,None) # error is stored in this vector after successful detection.
+    target_position=(0.5,0.5) # width,height in range 0 to 1.  Where do you want the center of the target in the frame?
+    allowed_error=(0.1,0.1) # width,height in range 0 to 1.   FHD 1920x1080, 0.1 corresponds to 192,108 pixel allowed error vectors.
+    
+    hafov = ptz.ptz_get_hfov() # horizontal angular field of view.
+    vafov = 9./16.*hafov # vertical angular field of view, assumes 16:9 aspect ratio and rectilinear lens.
+
+    # Where we want the target in the frame
+    target_loc = np.array((frame.shape[1]*target_position[0],frame.shape[0]*target_position[1]))
+
+    loop_rate=10 # starting value   
+    while(True):
+        tstart=time.time()
+        if haltTrackingThread(cam_ip):
+            return(0)
+
+        # To most closely sync the current position with the frame, it's best to get position first
+        # position getting has the most latency.
+        cam_abs_pos,frame_ts = ptz.ptz_get_position(return_ts=True) # About 50ms or so usually.
+        if cam_abs_pos==None: # Sometimes the socket flakes.
+            print("[ Warning ] - Cam abs position was none: ",cam_abs_pos)
+            continue
+
+        # Grab a matching frame right away - NDI frame grabbing is usually pretty quick, 5ms or so.
+        frame = ndi.get_cv2_frame()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame.setflags(write=1)
+
+        # Do inference.
+        actual_loc = detect_faces_mp(frame,detect_faces)
+        if actual_loc:
+            actual_loc=np.array(actual_loc)
+        else:
+            print("\r No face detected, passing",end=" ")
+            loop_rate = 1/(time.time()-tstart)
+            print("Loop rate: 2",loop_rate, end=" ")
+            continue
+
+        hafov = ptz.ptz_get_hfov() # horizontal angular field of view.
+        vafov = 9./16.*hafov # vertical angular field of view, assumes 16:9 aspect ratio and rectilinear lens.
+
+        error_vector = actual_loc-target_loc # Compute error vector.
+        permitted_error = allowed_error * np.array((frame.shape[1],frame.shape[0]))
+        dx = float(hafov*error_vector[0])/float(frame.shape[1]) # pan error in degrees
+        dy = -float(vafov*error_vector[1])/float(frame.shape[0]) # tilt error in degrees
+        cam_abs_pos = np.array(cam_abs_pos)
+        cam_correct_pos = cam_abs_pos + np.array((dx,dy))
+
+        # If we are outside the allowable error, command a correction.
+        if (abs(error_vector[0]) > permitted_error[0] or abs(error_vector[1]) > permitted_error[1]):
+            # we want our correction to take about 3x the loop time.
+            # Loop at 10hz means we want 0.3s in our movement command.
+            # To accomplish, compute angular movement and convert to cam speeds for pan and tilt.
+            pan_angular_error = abs(error_vector[0])/frame.shape[1]*hafov # degrees
+            tilt_angular_error = abs(error_vector[1])/frame.shape[0]*vafov # degrees
+            pan_speed = ptz.pan_deg_per_sec_to_speed(pan_angular_error*loop_rate/6.)
+            tilt_speed = ptz.tilt_deg_per_sec_to_speed(tilt_angular_error*loop_rate/6.)
+
+            ptz.send(ptz.cmd.ptz_to_abs_position(cam_correct_pos[0],cam_correct_pos[1],pan_speed,tilt_speed))
+
+        loop_rate=1/(time.time()-tstart)
+        print("\rLoop rate: ",loop_rate," Hz", end="")
+
+        
+
 def clip(value, min_val, max_val):
     return min(max(value, min_val), max_val)
+
+def detect_faces_mp(frame,detect_faces):
+    faces = detect_faces(frame)
+    if not len(faces):
+        return None
+    else:
+        points=[]
+        for f in faces:
+            if f.score>0.75:
+                bbox = f.bbox
+                bbox_center_xy_n = ((bbox.xmin+bbox.xmax)/2., (bbox.ymin+bbox.ymax)/2.)
+                bbox_center = (int(bbox_center_xy_n[0]*frame.shape[1]),int(bbox_center_xy_n[1]*frame.shape[0]))
+                points.append(bbox_center)
+
+        if len(points):
+            px=0; py=0
+            for p in points:
+                px+=p[0]
+                py+=p[1]
+            px = px/len(points)
+            py = py/len(points)
+            centroid=(px,py)
+            return centroid
+    return None
+
 
 def detect_single_face_yolov8(array,model_face):
     """
@@ -164,7 +274,7 @@ def detect_single_face_yolov8(array,model_face):
     return centroid
 
 def load_sources_from_db():
-    with app.ctx.db_env.begin(write=False) as txn:
+    with db_env.begin(write=False) as txn:
         sourcepickle = txn.get("sources".encode('utf-8'))
     
     if sourcepickle:
@@ -173,12 +283,9 @@ def load_sources_from_db():
         sources=None
         
     if not sources:
-        print("Sources invalid or None, creating empty source list...")
+        print("[ Warning ] - Sources invalid or None, creating empty source list...")
         sources=list()
         store_sources_to_db(sources)
-
-    else:
-        print("Sources is valid: ",sources)
 
     return sources
 
@@ -187,7 +294,7 @@ def store_sources_to_db(sources):
     # persist sources in db as pickle
     p = pickle.dumps(sources)
     
-    with app.ctx.db_env.begin(write=True) as txn:
+    with db_env.begin(write=True) as txn:
         txn.put("sources".encode('utf-8'),p)
         return True
     return False
@@ -206,19 +313,19 @@ def haltTrackingThread(cam_ip):
             cam_ip_found=True
             halt = item.get('haltTrackingThread')
         
-    print("Halt: ",halt)
-    print("Cam_IP_found: ",cam_ip_found)
+    #print("Halt: ",halt)
+    #print("Cam_IP_found: ",cam_ip_found)
 
     if halt==True or cam_ip_found==False:
-        print("Exiting tracking thread.")
+        print(" [ Notice ] - Exiting tracking thread.")
         return True
-    print("Continuing")
+    #print("Continuing")
     return False
 
 
 # Render the main user interface.
 @app.get("/")
-async def main(request):
+def main():
     template=None
 
     sources=load_sources_from_db()
@@ -258,10 +365,10 @@ async def main(request):
     template=template.replace("%SOURCES%",stable)
     template=template.replace("%SCRIPTS%",scripts)
 
-    return html(template)
+    return template
 
 @app.get("/addcambyip")
-async def addcambyip(request):
+def addcambyip():
      
      # Get the args
      camip = request.args.get("camip")
@@ -283,16 +390,16 @@ async def addcambyip(request):
         # Persist changes to database
         store_sources_to_db(sources)
 
-     return text(f"{camip} {camname}")
+     return f"{camip} {camname}"
 
 
 @app.get("/camlist")
-async def camlist(request):
+def camlist():
     sources=load_sources_from_db()
-    return json(sources)
+    return sources
 
 @app.get("/removesource")
-async def removesource(request):
+def removesource():
     sources=load_sources_from_db()
     idx=int(request.args.get("idx"))
     del sources[idx]
@@ -302,7 +409,7 @@ async def removesource(request):
     return redirect("/")
 
 @app.get("/preview-b64")
-async def getPreview(request):
+def getPreview():
     idx = request.args.get("idx")
     idx = int(idx)
     print("idx: ",idx)
@@ -329,13 +436,13 @@ async def getPreview(request):
     print("Returning for idx ",idx,": ",bool(b64_image))
     html_src_tag = f"""data:image/jpg;base64,{b64_image}"""
 
-    return text(html_src_tag)
+    return html_src_tag
 
 
 
 
 @app.get("/startTracking")
-async def startTracking(request):
+def startTracking():
 
     cam_ip=None
     idx=None
@@ -363,19 +470,19 @@ async def startTracking(request):
             
     
     if cam_ip is None or idx is None:
-        return text(f"Failure - No cameras found by that identifier: idx={idx} cam_ip={cam_ip}")
+        return f"Failure - No cameras found by that identifier: idx={idx} cam_ip={cam_ip}"
     
     # Spawn separate thread for tracking code
     sources[idx]['haltTrackingThread']=False
 
-    T = threading.Thread(target=track_single_without_zoom,args=(cam_ip,))
+    T = threading.Thread(target=mediapipe_tracker,args=(cam_ip,), daemon=True)
     T.start()
 
     store_sources_to_db(sources)
     return redirect("/")
 
 @app.get("/stopTracking")
-async def stopTracking(request):
+def stopTracking():
 
     cam_ip=None
     idx=None
@@ -406,14 +513,14 @@ async def stopTracking(request):
     store_sources_to_db(sources)
     return redirect("/")
 
-@app.get("searchndi")
-async def searchndi(request):
+@app.get("/searchndi")
+def searchndi():
     cam=AvkansCamera()
     found=cam.get_sources_as_dict()
-    return text(jsonlib.dumps(found,indent=2))
+    return jsonlib.dumps(found,indent=2)
 
 if __name__ == "__main__":
 
     # Use lightning db for inter-worker communication
     print("Opening database!!! ")
-    app.run(host="0.0.0.0",port=8091,debug=True,auto_reload=True,reload_dir=["./","./lib","./html"])
+    app.run(host="0.0.0.0",port=8091,debug=True)
